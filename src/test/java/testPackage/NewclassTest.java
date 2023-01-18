6f9ee178ee1841ae8cb7f40e3fdaf82dd8b2ee55
sharunbansal03 <sharun.bansal03@gmail.com>
2023-01-18 11:49:55 +0530

    commit by collaborator

--- src/test/java/testPackage/NewclassTest.java
+++ src/test/java/testPackage/NewclassTest.java
@@ -6,6 +6,7 @@ public class NewclassTest {
 	@Test
 	public void newTest() {
 		System.out.println("Class added by Lead");
+		System.out.println("changes made by collborator - sharunbansal04");
 	}
 
 }
