7283d1649b12e5ee67f13c2a201dcfd4fe47139c
User <User@SIMPLEBUDDY>
2023-01-18 14:55:49 +0530

    Commit by Rishabh

---
+++ src/test/java/testPackage/PracticeClass.java
@@ -0,0 +1,11 @@
+package testPackage;
+
+public class PracticeClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hii this is Rishabh's demo class");
+
+	}
+
+}
