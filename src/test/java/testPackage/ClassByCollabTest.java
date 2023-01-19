4234a26109c3cc4efce36c754e889d5422925ce4
sharunbansal04 <sharun.bansal00@gmail.com>
2023-01-19 11:19:50 +0530

    added class collab

---
+++ src/test/java/testPackage/ClassByCollabTest.java
@@ -0,0 +1,10 @@
+package testPackage;
+
+import org.testng.annotations.Test;
+
+public class ClassByCollabTest {
+	@Test
+	public void addedTest() {
+		System.out.println("Added test script to check conflict process");
+	}
+}
